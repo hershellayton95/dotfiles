94575dd9ca2005649165cf456fb6b93e4bd8af10
Filippo Di Marco <filippo.dimarco@activadigital.it>
2024-03-05 13:26:01 +0100

    feat(yay): add zsh functon

--- archi3_script.sh
+++ archi3_script.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
+source ./zsh/zsh
 source ./yay/yay
 source ./starship/starship
 
 sudo pacman -Syu
-sudo pacman -S --needed zsh base-devel git htop neofetch fontconfig
+sudo pacman -S --needed base-devel git htop neofetch fontconfig
 
-chsh -s zsh
-
+install_zsh
 installa_yay
 install_starship
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 yay -S --needed timeshift timeshift-autosnap neovim
-
-curl -sS https://starship.rs/install.sh | sh
