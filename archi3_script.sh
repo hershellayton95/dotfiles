c63c6f27ed283f819cbb9bfdb4c8190559f3a3a1
Filippo Di Marco <filippo.dimarco@activadigital.it>
2024-03-05 13:01:36 +0100

    feat(yay): update yay

--- archi3_script.sh
+++ archi3_script.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+source ./yay/yay
+
 sudo pacman -Syu
-sudo pacman -S --needed zsh base-devel git htop neofetch
-
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ..
-
-sudo rm -r yay
+sudo pacman -S --needed zsh base-devel git htop neofetch fontconfig
 
 chsh -s zsh
 
+installa_yay
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+yay -S timeshift timeshift-autosnap
+
+curl -sS https://starship.rs/install.sh | sh
