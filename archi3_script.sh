cef3a9375e497ce5edcff2ad2fe6736af834680f
Filippo Di Marco <filippo.dimarco@activadigital.it>
2024-03-05 09:43:38 +0100

    feat(yay): add install yayOD

--- archi3_script.sh
+++ archi3_script.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 sudo pacman -Syu
-sudo pacman -S zsh base-devel git htop neofetch
+sudo pacman -S --needed zsh base-devel git htop neofetch
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm yay
