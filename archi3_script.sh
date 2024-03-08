fa51402732dccf89dd73673da8d86b09ae6ff010
Filippo Di Marco <filippo.dimarco@activadigital.it>
2024-03-08 13:05:37 +0100

    add starship

--- archi3_script.sh
+++ archi3_script.sh
@@ -8,11 +8,18 @@ makepkg -si
 cd ..
 sudo rm -r yay
 
-yay -Sy --needed timeshift timeshift-autosnap neovim google-chrome vscodium-bin vscodium-bin-marketplace
-
-sudo pacman -S --needed git base-devel zsh htop neofetch fontconfig
+yay -Sy --needed timeshift timeshift-autosnap 
+sudo pacman -S --needed zsh htop neofetch fontconfig firefox pass
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -sS https://starship.rs/install.sh | sh
 
 chsh -s zsh
 
+yay -Sy --needed neovim google-chrome vscodium-bin vscodium-bin-marketplace 
+
+rm -r  ~/.zshrc
+rm -r ~/.oh-my-git
+
+cp -r ./zsh/ ~
+cp -r ./oh-my-git/ ~
+cp ./starship/ ~/.config/starship.toml
