e91439a5c376109ae32595f6d855cbdef7b6d64b
Filippo Di Marco <filippo.dimarco@activadigital.it>
2024-03-09 23:56:03 +0100

    add picom

--- arch_installer.sh
+++ arch_installer.sh
@@ -3,7 +3,8 @@
 sudo pacman -Sy --needed neovim \
 	alacritty \
 	brightnessctl \
-	timeshift
+	timeshift \
+	picom
 
 yay -Sy --needed xkblayout-state timeshift-autosnap
 
@@ -11,11 +12,13 @@ rm -r ~/.config/i3 2>/dev/null
 ln -s ~/dotfiles/i3 ~/.config/ 2>/dev/null
 rm -r ~/.config/rofi 2>/dev/null
 ln -s ~/dotfiles/rofi ~/.config/ 2>/dev/null
-rm -r ~/.zshrc 2>/dev/null
+rm ~/.zshrc 2>/dev/null
 ln -s ~/dotfiles/.zshrc ~ 2>/dev/null
 rm -r ~/.zsh 2>/dev/null
 ln -s ~/dotfiles/.zsh ~ 2>/dev/null
 rm -r ~/.config/alacritty 2>/dev/null
 ln -s ~/dotfiles/alacritty ~/.config/ 2>/dev/null
-rm -r ~/.gitconfig 2>/dev/null
-ln -s ~/dotfiles/gitconfig/.gitconfig ~/.gitconfig
+rm ~/.gitconfig 2>/dev/null
+ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig 2>/dev/null
+rm -r ~/config/picom 2>/dev/null
+ln -s ~/dotfiles/picom ~/.config/ 2>/dev/null
