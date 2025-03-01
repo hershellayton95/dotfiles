7c6590a9b3a0b6eec637ae87cb3e2d700b0eea64
Filippo Di Marco <filippo.dimarco@activadigital.it>
2025-03-01 20:08:31 +0100

    update arch i3 config

--- arch_installer.sh
+++ arch_installer.sh
@@ -1,56 +1,54 @@
 #!/usr/bin/env bash
-pacman_pkgs=(
-    neovim
-    zsh
-    alacritty
-    brightnessctl
-    timeshift
-    starship
-    picom
-    docker
-    docker-compose
-    podman
-    buildah
-    kubectl
-    xclip
-    xdotool
-    ydotool
-    xautolock
-    htop
-    gtk2
-    gtk3
-    gtk4
-    xournalpp
-    flameshot
-    dive
-    #code
+sudo pacman -Rc i3lock xfce4-terminal endeavouros-xfce4-terminal-colors
+
+sudo pacman -Sy --needed \
+	neovim \
+	zsh \
+	alacritty \
+	brightnessctl \
+	timeshift \
+	starship \
+	picom \
+	docker \
+	docker-compose \
+	podman \
+	buildah \
+	kubectl \
+	xclip \
+	xdotool \
+	ydotool \
+	xautolock \
+	htop \
+	gtk2 \
+	gtk3 \
+	gtk4 \
+	xournalpp \
+	flameshot \
+	dive \
     kleopatra
-)
-pacman -Sy --needed "${pacman_pkgs[@]}"
-
-# Installazione dei pacchetti AUR
-aur_pkgs=(
-    timeshift-autosnap
-    google-chrome
-    openlens-bin
-    openfortivpn-webview-qt
-    pass
-    passmenu
-    xkblayout-state
-    pamac-aur
-    i3lock-color
-    kind
-    #postman
-    rofi-greenclip
-    #volctl
-)
-yay -Sy --needed "${aur_pkgs[@]}"
+#	code \
 
 
-rm -r ~/.config/i3 2>/dev/null
-ln -s ~/dotfiles/i3 ~/.config/ 2>/dev/null
-rm -r ~/.config/rofi 2>/dev/null
-ln -s ~/dotfiles/rofi ~/.config/ 2>/dev/null
+
+yay -Sy --needed \
+	timeshift-autosnap \
+	google-chrome \
+	openfortivpn-webview-qt \
+	pass \
+	passmenu \
+	xkblayout-state \
+	pamac-aur \
+	i3lock-color \
+	kind \
+	rofi-greenclip
+#	postman \
+	#volctl
+
+#rm -r ~/.config/i3 2>/dev/null
+#ln -s ~/dotfiles/i3 ~/.config/ 2>/dev/null
+#rm -r ~/.config/rofi 2>/dev/null
+#ln -s ~/dotfiles/rofi ~/.config/ 2>/dev/null
+
 rm ~/.zshrc 2>/dev/null
 ln -s ~/dotfiles/.zshrc ~ 2>/dev/null
 rm -r ~/.zsh 2>/dev/null
@@ -63,11 +61,13 @@ rm -r ~/config/picom 2>/dev/null
 ln -s ~/dotfiles/picom ~/.config/ 2>/dev/null
 rm -r ~/.config/starship* 2>/dev/null
 ln -s ~/dotfiles/starship/* ~/.config/ 2>/dev/null
-rm -r ~/.config/autostart 2>/dev/null
-ln -s ~/dotfiles/autostart ~/.config/ 2>/dev/null
+
+#rm -r ~/.config/autostart 2>/dev/null
+#ln -s ~/dotfiles/autostart ~/.config/ 2>/dev/null
+
 rm -r ~/.config/mimeapps.list 2>/dev/null
 ln -s ~/dotfiles/mimeapps/mimeapps.list ~/.config/ 2>/dev/null
-rm -r ~/.config/nvim 2>/dev/null
+#rm -r ~/.config/nvim 2>/dev/null
 rm -r ~/.config/git 2>/dev/null
 ln -s ~/dotfiles/git/git ~/.config/ 2>/dev/null
 
@@ -75,5 +75,10 @@ ln -s ~/dotfiles/git/git ~/.config/ 2>/dev/null
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone -b custom https://github.com/hershellayton95/vecna-vim ~/.config/nvim 2>/dev/null
+
+git clone https://aur.archlinux.org/asdf-vm.git && cd asdf-vm && makepkg -si && rm -r asdf-vm
