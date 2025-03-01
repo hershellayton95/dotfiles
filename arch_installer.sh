1cd94f60e4465baaea4677ff9b01ed1e389b3cfd
Filippo Di Marco <filippo.dimarco@activadigital.it>
2025-03-02 00:00:26 +0100

    update i3

--- arch_installer.sh
+++ arch_installer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo pacman -Rc i3lock xfce4-terminal endeavouros-xfce4-terminal-colors
+sudo pacman -Rc i3lock
 
 sudo pacman -Sy --needed \
 	neovim \
@@ -44,32 +44,74 @@ yay -Sy --needed \
 #	postman \
 	#volctl
 
-#rm -r ~/.config/i3 2>/dev/null
-#ln -s ~/dotfiles/i3 ~/.config/ 2>/dev/null
-#rm -r ~/.config/rofi 2>/dev/null
-#ln -s ~/dotfiles/rofi ~/.config/ 2>/dev/null
+echo "rm -r ~/.config/i3 2>/dev/null"
+rm -r ~/.config/i3 2>/dev/null
 
+echo "ln -s ~/dotfiles/i3 ~/.config/ 2>/dev/null"
+ln -s ~/dotfiles/i3 ~/.config/ 2>/dev/null
+
+echo "rm -r ~/.config/rofi 2>/dev/null"
+rm -r ~/.config/rofi 2>/dev/null
+
+echo "ln -s ~/dotfiles/rofi ~/.config/ 2>/dev/null"
+ln -s ~/dotfiles/rofi ~/.config/ 2>/dev/null
+
+echo "rm ~/.zshrc 2>/dev/null"
 rm ~/.zshrc 2>/dev/null
+
+echo "ln -s ~/dotfiles/.zshrc ~ 2>/dev/null"
 ln -s ~/dotfiles/.zshrc ~ 2>/dev/null
+
+echo "rm -r ~/.zsh 2>/dev/null"
 rm -r ~/.zsh 2>/dev/null
+
+echo "ln -s ~/dotfiles/.zsh ~ 2>/dev/null"
 ln -s ~/dotfiles/.zsh ~ 2>/dev/null
+
+echo "rm -r ~/.config/alacritty 2>/dev/null"
 rm -r ~/.config/alacritty 2>/dev/null
+
+echo "ln -s ~/dotfiles/alacritty ~/.config/ 2>/dev/null"
 ln -s ~/dotfiles/alacritty ~/.config/ 2>/dev/null
+
+echo "rm ~/.gitconfig 2>/dev/null"
 rm ~/.gitconfig 2>/dev/null
+
+echo "ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig 2>/dev/null"
 ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig 2>/dev/null
+
+echo "rm -r ~/config/picom 2>/dev/null"
 rm -r ~/config/picom 2>/dev/null
+
+echo "ln -s ~/dotfiles/picom ~/.config/ 2>/dev/null"
 ln -s ~/dotfiles/picom ~/.config/ 2>/dev/null
+
+echo "rm -r ~/.config/starship* 2>/dev/null"
 rm -r ~/.config/starship* 2>/dev/null
+
+echo "ln -s ~/dotfiles/starship/* ~/.config/ 2>/dev/null"
 ln -s ~/dotfiles/starship/* ~/.config/ 2>/dev/null
+
+echo "rm -r ~/.config/mimeapps.list 2>/dev/null"
+rm -r ~/.config/mimeapps.list 2>/dev/null
+
+echo "ln -s ~/dotfiles/mimeapps/mimeapps.list ~/.config/ 2>/dev/null"
+ln -s ~/dotfiles/mimeapps/mimeapps.list ~/.config/ 2>/dev/null
+
+echo "sudo rm -r ~/.config/nvim 2>/dev/null"
+sudo rm -r ~/.config/nvim 2>/dev/null
+
+echo "rm -r ~/.config/git 2>/dev/null"
+rm -r ~/.config/git 2>/dev/null
+
+echo "ln -s ~/dotfiles/git/git ~/.config/ 2>/dev/null"
+ln -s ~/dotfiles/git/git ~/.config/ 2>/dev/null
+
 
 #rm -r ~/.config/autostart 2>/dev/null
 #ln -s ~/dotfiles/autostart ~/.config/ 2>/dev/null
 
-rm -r ~/.config/mimeapps.list 2>/dev/null
-ln -s ~/dotfiles/mimeapps/mimeapps.list ~/.config/ 2>/dev/null
-#rm -r ~/.config/nvim 2>/dev/null
-rm -r ~/.config/git 2>/dev/null
-ln -s ~/dotfiles/git/git ~/.config/ 2>/dev/null
+
 
 #nvim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
--- i3/scripts/volume_brightness.sh
+++ i3/scripts/volume_brightness.sh
@@ -43,18 +43,19 @@ function get_brightness_icon {
     brightness_icon=""
 }
 
-# Displays a volume notification using dunstify
+# Displays a volume notification using notify-send
 function show_volume_notif {
     volume=$(get_mute)
     get_volume_icon
-    dunstify -i audio-volume-muted-blocking -t 1000 -r 2593 -u normal "$volume_icon $volume%" -h int:value:$volume -h string:hlcolor:$bar_color
+    notify-send -i audio-volume-muted -t 1000 "Volume" "$volume_icon $volume%" -h int:value:$volume -h string:x-canonical-private-synchronous:volume
 }
 
 # Displays a brightness notification using dunstify
 function show_brightness_notif {
     brightness=$(get_brightness)
+    echo $brightness
     get_brightness_icon
-    dunstify -t 1000 -r 2593 -u normal "$brightness_icon $brightness%" -h int:value:$brightness -h string:hlcolor:$bar_color
+    notify-send -t $notification_timeout -h string:x-dunst-stack-tag:brightness_notif -h int:value:$brightness "$brightness_icon $brightness%"
 }
 
 # Main function - Takes user input, "volume_up", "volume_down", "brightness_up", or "brightness_down"
@@ -84,12 +85,16 @@ case $1 in
     ;;
 
     brightness_up)
+    ## Increases brightness and displays the notification
+    #xbacklight -A $brightness_step 
     # Increases brightness using brightnessctl
     brightnessctl set +$brightness_step%
     show_brightness_notif
     ;;
 
     brightness_down)
+    # # Decreases brightness and displays the notification
+    # xbacklight -U $brightness_step
     # Decreases brightness using brightnessctl
     if [ $(brightnessctl -m | grep -Eo '[0-9]+' | head -n 2 | tail  -n 1) -gt $brightness_step ]; then
         brightnessctl set $brightness_step%-
