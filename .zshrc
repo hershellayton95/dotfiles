a40af763b8376c00d5f6d0db2c56d56132dba334
Filippo Di Marco <filidm5795@gmail.com>
2026-01-19 15:26:49 +0100

    update dotfiles

--- .zshrc
+++ .zshrc
@@ -1,39 +1,4 @@
 source $HOME/.zsh/zshrc
 source $HOME/.zsh/zsh_env
 
-# Generated for envman. Do not edit.
-[ -s "$HOME/.config/envman/load.sh" ] && source "$HOME/.config/envman/load.sh"
 
-# Added by LM Studio CLI (lms)
-export PATH="$PATH:/home/filippo/.lmstudio/bin"
-# End of LM Studio CLI section
-
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/filippo/miniforge3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home/filippo/miniforge3/etc/profile.d/conda.sh" ]; then
-        . "/home/filippo/miniforge3/etc/profile.d/conda.sh"
-    else
-        export PATH="/home/filippo/miniforge3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
-
-
-# >>> mamba initialize >>>
-# !! Contents within this block are managed by 'mamba shell init' !!
-export MAMBA_EXE='/home/filippo/miniforge3/bin/mamba';
-export MAMBA_ROOT_PREFIX='/home/filippo/miniforge3';
-__mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__mamba_setup"
-else
-    alias mamba="$MAMBA_EXE"  # Fallback on help from mamba activate
-fi
-unset __mamba_setup
-# <<< mamba initialize <<<
---
+++ arch_wsl.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+
+set -x
+
+sudo pacman -Syy
+
+sudo pacman -S --needed --noconfirm \
+    git \
+    zsh \
+    curl \
+    base-devel 
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -sS https://starship.rs/install.sh | sh
+
+if ! command -v yay &> /dev/null; then
+    echo "yay non trovato, installazione in corso..."
+    # Creiamo una cartella temporanea per la build
+    _temp_dir=$(mktemp -d)
+    git clone https://aur.archlinux.org/yay.git "$_temp_dir/yay"
+    pushd "$_temp_dir/yay"
+    makepkg -si --noconfirm
+    popd
+    rm -rf "$_temp_dir"
+fi
+
+yay -S --needed --noconfirm asdf-vm nvim ttf-firacode-nerd
+
+sudo pacman -S --needed --noconfirm \
+    k9s \
+    kind \
+    nvim \
+    btop \
+    htop \
+    kubectl \
+    net-tools \
+    docker \
+    podman \
+    docker-compose \
+    podman-compose \
+    meld \
+    xclip \
+    openssl \
+    buildah \
+    helm \
+    krew \
+    skopeo \
+    chromium \
+    kustomize \
+    zoxide \
+    shellcheck \
+    atuin
+    
+sudo sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sudo locale-gen
+echo "LANG=en_US.UTF-8" | sudo tee /etc/locale.conf
+
+
+echo "rm ~/.zshrc 2>/dev/null"
+rm ~/.zshrc 2>/dev/null
+
+echo "ln -s ~/dotfiles/.zshrc ~ 2>/dev/null"
+ln -s ~/dotfiles/.zshrc ~ 2>/dev/null
+
+echo "rm -r ~/.zsh 2>/dev/null"
+rm -r ~/.zsh 2>/dev/null
+
+echo "ln -s ~/dotfiles/.zsh ~ 2>/dev/null"
+ln -s ~/dotfiles/.zsh ~ 2>/dev/null
+
+echo "rm ~/.gitconfig 2>/dev/null"
+rm ~/.gitconfig 2>/dev/null
+
+echo "ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig 2>/dev/null"
+ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig 2>/dev/null
+
+echo "rm -r ~/.config/starship* 2>/dev/null"
+rm -r ~/.config/starship* 2>/dev/null
+
+echo "ln -s ~/dotfiles/starship/* ~/.config/ 2>/dev/null"
+ln -s ~/dotfiles/starship/* ~/.config/ 2>/dev/null
+
+echo "sudo rm -r ~/.config/nvim 2>/dev/null"
+sudo rm -r ~/.config/nvim 2>/dev/null
+
+echo "rm -r ~/.config/git 2>/dev/null"
+rm -r ~/.config/git 2>/dev/null
+
+echo "ln -s ~/dotfiles/git/git ~/.config/ 2>/dev/null"
+ln -s ~/dotfiles/git/git ~/.config/ 2>/dev/null
+
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search &> /dev/null;
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions &> /dev/null;
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &> /dev/null;
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &> /dev/null;
+
+asdf plugin-add crane https://github.com/dmpe/asdf-crane.git
+asdf plugin-add dive https://github.com/looztra/asdf-dive
+asdf install
+
+
+helm plugin install https://github.com/chartmuseum/helm-push
+
+kubectl krew update
+kubectl krew install neat
+kubectl krew install oidc-login
