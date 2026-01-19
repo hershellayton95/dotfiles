a4bf840d9e762a9f526889bc31b39142db700b62
hershellayton <filidm5795@gmail.com>
2026-01-19 15:21:18 +0100

    update dotfiles

--- arch_installer.sh
+++ arch_installer.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-sudo pacman -Rc i3lock
+# sudo pacman -Rc i3lock
 
 sudo pacman -Sy --needed \
 	neovim \
 	zsh \
-	alacritty \
-	brightnessctl \
 	timeshift \
 	starship \
 	picom \
@@ -15,47 +13,48 @@ sudo pacman -Sy --needed \
 	buildah \
 	kubectl \
 	xclip \
-	xdotool \
-	ydotool \
-	xautolock \
 	htop \
-	gtk2 \
-	gtk3 \
-	gtk4 \
 	xournalpp \
 	flameshot \
 	dive \
     qtpass \
     kleopatra
 #	code \
-
-
+# 	gtk2 \
+# 	gtk3 \
+# 	gtk4 \
+# 	xdotool \
+# 	ydotool \
+# 	xautolock \
+# 	alacritty \
+# 	brightnessctl \
 
 yay -Sy --needed \
 	timeshift-autosnap \
 	google-chrome \
-	openfortivpn-webview-qt \
+# 	openfortivpn-webview-qt \
 	pass \
 	passmenu \
-	xkblayout-state \
-	pamac-aur \
-	i3lock-color \
-	kind \
-	rofi-greenclip
+	kind
+# 	openfortivpn-webview-qt \
+# 	xkblayout-state \
+# 	pamac-aur \
+# 	i3lock-color \
+# 	rofi-greenclip
 #	postman \
 	#volctl
 
-echo "rm -r ~/.config/i3 2>/dev/null"
-rm -r ~/.config/i3 2>/dev/null
-
-echo "ln -s ~/dotfiles/i3 ~/.config/ 2>/dev/null"
-ln -s ~/dotfiles/i3 ~/.config/ 2>/dev/null
-
-echo "rm -r ~/.config/rofi 2>/dev/null"
-rm -r ~/.config/rofi 2>/dev/null
-
-echo "ln -s ~/dotfiles/rofi ~/.config/ 2>/dev/null"
-ln -s ~/dotfiles/rofi ~/.config/ 2>/dev/null
+# echo "rm -r ~/.config/i3 2>/dev/null"
+# rm -r ~/.config/i3 2>/dev/null
+#
+# echo "ln -s ~/dotfiles/i3 ~/.config/ 2>/dev/null"
+# ln -s ~/dotfiles/i3 ~/.config/ 2>/dev/null
+#
+# echo "rm -r ~/.config/rofi 2>/dev/null"
+# rm -r ~/.config/rofi 2>/dev/null
+#
+# echo "ln -s ~/dotfiles/rofi ~/.config/ 2>/dev/null"
+# ln -s ~/dotfiles/rofi ~/.config/ 2>/dev/null
 
 echo "rm ~/.zshrc 2>/dev/null"
 rm ~/.zshrc 2>/dev/null
@@ -69,51 +68,51 @@ rm -r ~/.zsh 2>/dev/null
 echo "ln -s ~/dotfiles/.zsh ~ 2>/dev/null"
 ln -s ~/dotfiles/.zsh ~ 2>/dev/null
 
-echo "rm -r ~/.config/alacritty 2>/dev/null"
-rm -r ~/.config/alacritty 2>/dev/null
+# echo "rm -r ~/.config/alacritty 2>/dev/null"
+# rm -r ~/.config/alacritty 2>/dev/null
+#
+# echo "ln -s ~/dotfiles/alacritty ~/.config/ 2>/dev/null"
+# ln -s ~/dotfiles/alacritty ~/.config/ 2>/dev/null
 
-echo "ln -s ~/dotfiles/alacritty ~/.config/ 2>/dev/null"
-ln -s ~/dotfiles/alacritty ~/.config/ 2>/dev/null
-
-echo "rm ~/.gitconfig 2>/dev/null"
-rm ~/.gitconfig 2>/dev/null
-
-echo "ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig 2>/dev/null"
-ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig 2>/dev/null
-
-echo "rm -r ~/config/picom 2>/dev/null"
-rm -r ~/config/picom 2>/dev/null
-
-echo "ln -s ~/dotfiles/picom ~/.config/ 2>/dev/null"
-ln -s ~/dotfiles/picom ~/.config/ 2>/dev/null
+# echo "rm ~/.gitconfig 2>/dev/null"
+# rm ~/.gitconfig 2>/dev/null
+#
+# echo "ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig 2>/dev/null"
+# ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig 2>/dev/null
+#
+# echo "rm -r ~/config/picom 2>/dev/null"
+# rm -r ~/config/picom 2>/dev/null
+#
+# echo "ln -s ~/dotfiles/picom ~/.config/ 2>/dev/null"
+# ln -s ~/dotfiles/picom ~/.config/ 2>/dev/null
 
 echo "rm -r ~/.config/starship* 2>/dev/null"
 rm -r ~/.config/starship* 2>/dev/null
 
 echo "ln -s ~/dotfiles/starship/* ~/.config/ 2>/dev/null"
 ln -s ~/dotfiles/starship/* ~/.config/ 2>/dev/null
-
-echo "rm -r ~/.config/mimeapps.list 2>/dev/null"
-rm -r ~/.config/mimeapps.list 2>/dev/null
-
-echo "ln -s ~/dotfiles/mimeapps/mimeapps.list ~/.config/ 2>/dev/null"
-ln -s ~/dotfiles/mimeapps/mimeapps.list ~/.config/ 2>/dev/null
+#
+# echo "rm -r ~/.config/mimeapps.list 2>/dev/null"
+# rm -r ~/.config/mimeapps.list 2>/dev/null
+#
+# echo "ln -s ~/dotfiles/mimeapps/mimeapps.list ~/.config/ 2>/dev/null"
+# ln -s ~/dotfiles/mimeapps/mimeapps.list ~/.config/ 2>/dev/null
 
 echo "sudo rm -r ~/.config/nvim 2>/dev/null"
 sudo rm -r ~/.config/nvim 2>/dev/null
+#
+# echo "rm -r ~/.config/git 2>/dev/null"
+# rm -r ~/.config/git 2>/dev/null
+#
+# echo "ln -s ~/dotfiles/git/git ~/.config/ 2>/dev/null"
+# ln -s ~/dotfiles/git/git ~/.config/ 2>/dev/null
 
-echo "rm -r ~/.config/git 2>/dev/null"
-rm -r ~/.config/git 2>/dev/null
-
-echo "ln -s ~/dotfiles/git/git ~/.config/ 2>/dev/null"
-ln -s ~/dotfiles/git/git ~/.config/ 2>/dev/null
-
-
-echo "rm -r ~/.config/autostart 2>/dev/null"
-rm -r ~/.config/autostart 2>/dev/null
-
-echo "ln -s ~/dotfiles/autostart ~/.config/ 2>/dev/null"
-ln -s ~/dotfiles/autostart ~/.config/ 2>/dev/null
+#
+# echo "rm -r ~/.config/autostart 2>/dev/null"
+# rm -r ~/.config/autostart 2>/dev/null
+#
+# echo "ln -s ~/dotfiles/autostart ~/.config/ 2>/dev/null"
+# ln -s ~/dotfiles/autostart ~/.config/ 2>/dev/null
 
 #nvim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
