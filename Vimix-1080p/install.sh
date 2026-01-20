505985472a2047768912615a99b717b2312eb7d3
Filippo Di Marco <filidm5795@gmail.com>
2026-01-20 17:36:52 +0100

    fix vimix

--- Vimix-1080p/install.sh
+++ Vimix-1080p/install.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# Grub2 Theme
+# Grub2 Theme Installer - Corrected for EndeavourOS/Arch
 
 ROOT_UID=0
 THEME_DIR="/usr/share/grub/themes"
-THEME_NAME=Vimix
+THEME_NAME="Vimix" # Assicurati che questa cartella esista dove lanci lo script
 
-MAX_DELAY=20                                        # max delay for user to enter root password
+MAX_DELAY=20
 
 #COLORS
-CDEF=" \033[0m"                                     # default color
-CCIN=" \033[0;36m"                                  # info color
-CGSC=" \033[0;32m"                                  # success color
-CRER=" \033[0;31m"                                  # error color
-CWAR=" \033[0;33m"                                  # waring color
-b_CDEF=" \033[1;37m"                                # bold default color
-b_CCIN=" \033[1;36m"                                # bold info color
-b_CGSC=" \033[1;32m"                                # bold success color
-b_CRER=" \033[1;31m"                                # bold error color
-b_CWAR=" \033[1;33m"                                # bold warning color
+CDEF="\033[0m"                                  # default color
+CCIN="\033[0;36m"                               # info color
+CGSC="\033[0;32m"                               # success color
+CRER="\033[0;31m"                               # error color
+CWAR="\033[0;33m"                               # warning color
+b_CDEF="\033[1;37m"                             # bold default color
+b_CCIN="\033[1;36m"                             # bold info color
+b_CGSC="\033[1;32m"                             # bold success color
+b_CRER="\033[1;31m"                             # bold error color
+b_CWAR="\033[1;33m"                             # bold warning color
 
 # echo like ...  with  flag type  and display message  colors
 prompt () {
@@ -37,18 +37,25 @@ prompt () {
   esac
 }
 
-# Welcome message
-prompt -s "\n\t*************************\n\t*  ${THEME_NAME} - Grub2 Theme  *\n\t*************************"
-
-# Check command avalibility
+# Check command availability
 function has_command() {
   command -v $1 > /dev/null
 }
 
+# Welcome message
+prompt -s "\n\t*************************\n\t* ${THEME_NAME} - Grub2 Theme  *\n\t*************************"
+
 prompt -w "\nChecking for root access...\n"
 
-# Checking for root access and proceed if it is present
+# Checking for root access
 if [ "$UID" -eq "$ROOT_UID" ]; then
+
+  # --- CHECK 1: Verify source directory exists (CRITICAL FIX) ---
+  if [ ! -d "./${THEME_NAME}" ]; then
+    prompt -e "\n[ Error! ] -> Directory './${THEME_NAME}' not found in current folder."
+    prompt -w "Please run this script from the folder containing the '${THEME_NAME}' directory."
+    exit 1
+  fi
 
   # Create themes directory if not exists
   prompt -i "\nChecking for the existence of themes directory...\n"
@@ -56,48 +63,8 @@ if [ "$UID" -eq "$ROOT_UID" ]; then
   mkdir -p "${THEME_DIR}/${THEME_NAME}"
 
   # Copy theme
-  prompt -i "\nInstalling ${THEME_NAME} theme...\n"
-
-  cp -a ${THEME_NAME}/* ${THEME_DIR}/${THEME_NAME}
+  prompt -i "\nInstalling ${THEME_NAME} theme to ${THEME_DIR}...\n"
+  cp -a ./${THEME_NAME}/* ${THEME_DIR}/${THEME_NAME}
 
   # Set theme
-  prompt -i "\nSetting ${THEME_NAME} as default...\n"
-
-  # Backup grub config
-  cp -an /etc/default/grub /etc/default/grub.bak
-
-  grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null && sed -i '/GRUB_THEME=/d' /etc/default/grub
-
-  echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >> /etc/default/grub
-
-  # Update grub config
-  echo -e "Updating grub config..."
-  if has_command update-grub; then
-    update-grub
-  elif has_command grub-mkconfig; then
-    grub-mkconfig -o /boot/grub/grub.cfg
-  elif has_command grub2-mkconfig; then
-    if has_command zypper; then
-      grub2-mkconfig -o /boot/grub2/grub.cfg
-    elif has_command dnf; then
-      grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
-    fi
-  fi
-
-  # Success message
-  prompt -s "\n\t          ***************\n\t          *  All done!  *\n\t          ***************\n"
-
-else
-
-  # Error message
-  prompt -e "\n [ Error! ] -> Run me as root "
-
-  # persisted execution of the script as root
-  read -p "[ trusted ] specify the root password : " -t${MAX_DELAY} -s
-  [[ -n "$REPLY" ]] && {
-    sudo -S <<< $REPLY $0
-  } || {
-    prompt  "\n Operation canceled  Bye"
-    exit 1
-  }
-fi
+  prompt -i "\nSetting ${THEME_NAME} as default in /etc/default/grub...\
