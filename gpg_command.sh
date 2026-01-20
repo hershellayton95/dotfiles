bf6261df0b252d96683fc2848e43de214d50c860
Filippo Di Marco <filidm5795@gmail.com>
2026-01-20 12:09:06 +0100

    update dotfiles with gpg

---
+++ gpg_command.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# gpg --import ~/private-key.{gpg,asc,cer}
+# gpg --list-keys
+
+# ID Lungo della chiave
+# gpg --list-keys --keyid-format LONG ID_CHIAVE or EMAIL
+
+#vedere cert pub key
+# gpg --export --armor ID_CHIAVE or EMAIL
+
+#geneare cert di revoca
+# gpg --output mio_certificato_revoca.asc --gen-revoke ID_CHIAVE or EMAIL
+#import cert di revoca
+# gpg --import mio_certificato_revoca.asc
+
+# prendere gpg pub from keyserver ubuntu or hkps://keys.openpgp.org
+# gpg --keyserver hkps://keyserver.ubuntu.com \
+#     --keyserver-options "timeout=40 http-proxy=http://proxy.example.com:8080" \
+#     --recv-keys 0A5174AF
+
+# prendere gpg cercandola from keyserver hkps://keys.openpgp.org
+# gpg --keyserver hkps://keys.openpgp.org --search-keys ID_CHIAVE or EMAIL
+
+# Run: gpg --edit-key [KeyID or Email]
+# Type trust at the prompt.
+# Select 5 (I trust ultimately) if it is your own key.
+# Type save.
