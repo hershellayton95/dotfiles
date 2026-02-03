14b723f9256b22fc2542e81b2ef1eccbd5da81a2
Filippo Di Marco <filidm5795@gmail.com>
2026-02-03 17:55:27 +0100

    add seled secret

--- saled_secret.sh
+++ saled_secret.sh
@@ -12,3 +12,5 @@ fi
 curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION}/kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz"
 tar -xvzf kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz kubeseal
 sudo install -m 755 kubeseal /usr/local/bin/kubeseal
+
+rm kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz kubeseal
